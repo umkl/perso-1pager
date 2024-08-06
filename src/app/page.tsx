a2c5adb91b9e3b196c78367b6b5f75330d77fec4
Michael <michael.ungar03@gmail.com>
2024-08-06 15:26:56 +0200

    actual center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-	return <h1 className="font-bold top-1/2 left-1/2 absolute text-4xl">👋</h1>;
+	return (
+		<h1 className="font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute text-4xl text-center">
+			👋
+		</h1>
+	);
 }
